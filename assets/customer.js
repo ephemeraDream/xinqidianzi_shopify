c7798db656f088212b7d254996ef1f77eeb323a0
wuliheng <canggou.wulh@menggejk.com>
2025-01-24 20:28:27 +0800

    best sellers

--- assets/customer.js
+++ assets/customer.js
@@ -83,28 +83,3 @@ class CustomerAddresses {
     }
   };
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper('.best-sellers-swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-    slidesPerView: 2,
-
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-  });
-});
